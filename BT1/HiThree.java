12d5928789a299724439f06a4b4547553ca21357
minhminhhn <78309372+minhminhhn@users.noreply.github.com>
2021-09-03 22:27:39 +0700

    Add files via upload

---
+++ BT1/HiThree.java
@@ -0,0 +1,8 @@
+public class HiThree {
+    public static void main(String[] args){
+        System.out.print("Hi ");
+        for(int i=2; i>=0; i--){
+            System.out.print(args[i]+" ");
+        }
+    }
+}
